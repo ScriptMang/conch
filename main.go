f342cfbfebc7061a7d2c6150bdde3961bf48a2e7
Andy Peralta <aperalta686@gmail.com>
2024-04-28 00:31:52 -0400

    Add a page that'll perform a read operation (wip)
    - added a page that'll perform a read operation on the bikeshop
    database
     - deatils of said query maybe seen in the source files
    - there is a link between the index and this page
    - this page is still wip

--- main.go
+++ main.go
@@ -13,10 +13,18 @@ func main() {
 	r.LoadHTMLGlob("templates/*")
 	r.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl", gin.H{
-			"title": "Conch",
+			"title": "Conch Index-Page",
+			"crud1": "Perform a Read Operation ",
 		})
 	})
 
+	r.GET("/crud1", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "crud1.tmpl", gin.H{
+			"title":   "Crud1",
+			"details": "Request the First Name, Last Name, Price and Quantity of all the customers who's invoice has a unit price over $13 ",
+			"rslt":    "Query Value",
+		})
+	})
 	db.New()
 	r.Run()
 
