ab877616945275eb1cb388e3cc8822a045603ccf
Andy Peralta <aperalta686@gmail.com>
2024-04-30 14:42:17 -0400

    Query bikeshop database wip
    - was able to connect and query the database
    - still need to be able to return the result via the route handlers

--- internal/bikeshop/bikehshop.go
+++ internal/bikeshop/bikehshop.go
@@ -6,12 +6,33 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/georgysavva/scany/v2/pgxscan"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
+type Invoice struct {
+	Fname    string  `json:"fname"`
+	Lname    string  `json:"lname"`
+	Product  string  `json:"product"`
+	Price    float32 `json:"price"`
+	Quantity int     `json:"quantity"`
+	Category string  `json:"category"`
+	Shipping string  `json:"shipping"`
+}
+
+type invoices []*Invoice
+
+func (invs invoices) string() string {
+	str := ""
+	for _, inv := range invs {
+		str += fmt.Sprintf("%v\n", *inv)
+	}
+	return str
+}
+
 func New() {
 
-	uri := "postgres://username@localhost:5432/BikeShop"
+	uri := "postgres://username@localhost:5432/bikeshop"
 	os.Setenv("DATABASE_URL", uri)
 	ctx := context.Background()
 
@@ -20,7 +41,15 @@ func New() {
 		fmt.Fprintf(os.Stderr, "Unable to connect to a database: %v\n", err)
 		os.Exit(1)
 	}
+
 	defer db.Close()
 
-	fmt.Println("This is a msg directly from Bikeshop")
+	var invs invoices
+	if err := pgxscan.Select(ctx, db, &invs, `SELECT fname, lname,
+        product, price, quantity FROM invoices`); err != nil {
+		fmt.Fprintf(os.Stderr, "Query or row processing error: %v\n", err)
+	}
+
+	fmt.Printf("All the invoices\n")
+	fmt.Println(invs)
 }
