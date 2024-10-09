39e664f68c99aa49ff9bab6f799e858ab90c7741
Andy Peralta <aperalta686@gmail.com>
2024-10-09 19:58:48 -0400

    Modify bikeshop database to support user logins
    - add support of user logins in bikeshop database
     - added users table
     - refactored invoices table to include foreign key to users's
       table id attribute and have it cascade on delete

--- bikeshop.sql
+++ bikeshop.sql
@@ -2,8 +2,8 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 16.3 (Homebrew)
--- Dumped by pg_dump version 16.3 (Homebrew)
+-- Dumped from database version 16.4 (Homebrew)
+-- Dumped by pg_dump version 16.4 (Homebrew)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -21,25 +21,26 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
--- Name: invoices; Type: TABLE; Schema: public; Owner: user
+-- Name: invoices; Type: TABLE; Schema: public; Owner: <username>
 --
 
 CREATE TABLE public.invoices (
     id integer NOT NULL,
+    usr_id integer,
     fname character varying(50) NOT NULL,
     lname character varying(50) NOT NULL,
     product character varying(255) NOT NULL,
-    price numeric(4,2) NOT NULL,
+    price numeric(5,2) NOT NULL,
     quantity integer NOT NULL,
     category character varying(255) NOT NULL,
     shipping character varying(255) NOT NULL
 );
 
 
-ALTER TABLE public.invoices OWNER TO user;
+ALTER TABLE public.invoices OWNER TO <username>;
 
 --
--- Name: invoices_id_seq; Type: SEQUENCE; Schema: public; Owner: user
+-- Name: invoices_id_seq; Type: SEQUENCE; Schema: public; Owner: <username>
 --
 
 CREATE SEQUENCE public.invoices_id_seq
@@ -51,49 +52,116 @@ CREATE SEQUENCE public.invoices_id_seq
     CACHE 1;
 
 
-ALTER SEQUENCE public.invoices_id_seq OWNER TO user;
+ALTER SEQUENCE public.invoices_id_seq OWNER TO <username>;
 
 --
--- Name: invoices_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: user
+-- Name: invoices_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: <username>
 --
 
 ALTER SEQUENCE public.invoices_id_seq OWNED BY public.invoices.id;
 
 
 --
--- Name: invoices id; Type: DEFAULT; Schema: public; Owner: user
+-- Name: users; Type: TABLE; Schema: public; Owner: <username>
+--
+
+CREATE TABLE public.users (
+    id integer NOT NULL,
+    username character varying(80),
+    password character varying(255)
+);
+
+
+ALTER TABLE public.users OWNER TO <username>;
+
+--
+-- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: <username>
+--
+
+CREATE SEQUENCE public.users_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.users_id_seq OWNER TO <username>;
+
+--
+-- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: <username>
+--
+
+ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
+
+
+--
+-- Name: invoices id; Type: DEFAULT; Schema: public; Owner: <username>
 --
 
 ALTER TABLE ONLY public.invoices ALTER COLUMN id SET DEFAULT nextval('public.invoices_id_seq'::regclass);
 
 
 --
--- Data for Name: invoices; Type: TABLE DATA; Schema: public; Owner: user
+-- Name: users id; Type: DEFAULT; Schema: public; Owner: <username>
 --
 
-COPY public.invoices (id, fname, lname, product, price, quantity, category, shipping) FROM stdin;
-1	Dante	Ferges	Safety Goggles	15.99	3	Safety Equipment	423 Elm St, Chicago IL 60629
-2	Michael	Wither	Lubricant	11.99	1	Maintenance	230 Furginson Rd, Oklahoma OK 73102
-3	Georgei	Ventalin	Door Hinges	12.50	5	Home Improvement	495 Durvington Ave, Topeka KS 66603
-4	Edart	Muskrat	Wrench	24.99	1	Plumbing	654 Ulysses Ave, Oklahoma OK 73103
-5	Abra	Katern	DiscoBall	19.99	6	Party	829 Sherbert St, Portland ME 04102
-6	Charles	Tarly	Zombie Book	14.99	2	Fiction	134 Pluton St, Boston MA 02108
+ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
+
+
+--
+-- Data for Name: invoices; Type: TABLE DATA; Schema: public; Owner: <username>
+--
+
+COPY public.invoices (id, usr_id, fname, lname, product, price, quantity, category, shipping) FROM stdin;
 \.
 
 
 --
--- Name: invoices_id_seq; Type: SEQUENCE SET; Schema: public; Owner: user
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: <username>
 --
 
-SELECT pg_catalog.setval('public.invoices_id_seq', 6, true);
+COPY public.users (id, username, password) FROM stdin;
+\.
 
 
 --
--- Name: invoices invoices_pkey; Type: CONSTRAINT; Schema: public; Owner: user
+-- Name: invoices_id_seq; Type: SEQUENCE SET; Schema: public; Owner: <username>
+--
+
+SELECT pg_catalog.setval('public.invoices_id_seq', 1, false);
+
+
+--
+-- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: <username>
+--
+
+SELECT pg_catalog.setval('public.users_id_seq', 1, false);
+
+
+--
+-- Name: invoices invoices_pkey; Type: CONSTRAINT; Schema: public; Owner: <username>
 --
 
 ALTER TABLE ONLY public.invoices
     ADD CONSTRAINT invoices_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: <username>
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: invoices invoices_usr_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: <username>
+--
+
+ALTER TABLE ONLY public.invoices
+    ADD CONSTRAINT invoices_usr_id_fkey FOREIGN KEY (usr_id) REFERENCES public.users(id) ON DELETE CASCADE;
 
 
 --
